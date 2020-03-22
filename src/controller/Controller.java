9421926a0410806330d30f7b9a237019a87dc256
William Larsson <oi17wln@cs.umu.se>
2020-03-22 20:03:03 +0100

    Added comments and javadoc

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -4,39 +4,70 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.util.concurrent.ExecutionException;
 
+/**
+ * This is the main controller that sits in between the Model classes and
+ * the view, directing the input from the view to model classes and finally
+ * back to the view with new and updated data.
+ *
+ *
+ * Tableau: "http://api.sr.se/v2/scheduledepisodes?channelid=164", primaryNode: "scheduledepisode"
+ * Channels: "http://api.sr.se/v2/channels/? ", primaryNode: "channel"
+ */
 public class Controller {
     private UserInterface ui;
     private Worker worker = null;
     private final static String AppName = "RadioInfo";
-    private final static String APIUrl = "http://api.sr.se/v2/channels/?";
+    private final static String APIChannelsUrl = "http://api.sr.se/v2/channels/?";
+    private final static String APIScheduleUrl = "http://api.sr.se/v2/scheduledepisodes?channelid=";
 
 
+    /**
+     * Init the controller and graphical user interface.
+     */
     public Controller(){
         ui = new UserInterface(AppName);
         startWorkerTimer();
         //TODO: continue to rebuild the controller and the new Worker class.
     }
 
+
+    /**
+     * Immediately start a worker that will update the
+     * application and run this same method once every hour.
+     */
     private void startWorkerTimer(){
         Timer timer = new Timer(60 * 60 * 1000, this::startWorker);
         timer.setInitialDelay(0);
         timer.start();
     }
 
+
+    /**
+     * If there is no worker currently running, start a new
+     * one to update the ui.
+     * @param event = The timer or user input.
+     */
     private void startWorker(ActionEvent event){
         if (worker == null){
-            worker = new Worker(this::executeWorker);
+            worker = new Worker(this::updateUI);
             worker.execute();
         }
     }
 
-    private void executeWorker() {
+
+    /**
+     * Update the ui with the latest information.
+     * Is used together with the functional interface used
+     * by the swing workers.
+     */
+    private void updateUI() {
         try {
-            Object[][] data = worker.get(); //Waits if necessary for the computation to complete, and then retrieves its result.
+            Object[][] data = worker.get(); //Waits for the computation to complete, then gets the result.
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
         }
     }
+
 
     /**
      * Setup button listeners for the UI components.
